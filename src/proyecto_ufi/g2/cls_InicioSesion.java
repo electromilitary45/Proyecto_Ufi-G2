86e78a278131293abb94b4f347cbf863505538f1
Sebastian Villalobos <dereklevilla45@gmail.com>
2022-06-26 17:34:38 -0600

    Create cls_InicioSesion.java

---
+++ src/proyecto_ufi/g2/cls_InicioSesion.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto_ufi.g2;
+
+/**
+ *
+ * @author Derek Leiva
+ */
+public class cls_InicioSesion {
+    
+    //--------Atributos------
+    private String userIni, passwordIni;
+    private boolean ini;
+    
+    //-------Constructures----
+    public cls_InicioSesion(){
+        
+    }
+    
+    
+}
